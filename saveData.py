c40b95b98287aefd168f56e3c4c2c1c59b572b0b
wei15 <wei_li2@brown.edu>
2020-12-02 02:40:32 -0500

    update the saveData file

--- saveData.py
+++ saveData.py
@@ -1,43 +1,46 @@
 import numpy as np
+import random
 import math
 from game import Game
 
 def main(): 
-    # model = Game()
-    # a = model.action_random_true()
-    # print("a: ")
-    # print(a)
-    # print()
-
-    # b = model.action_random_true()
-    # print("b: ")
-    # print(b)
-    # print()
-
-    # c = np.array(a+b, dtype = object)
-
-    # np.save("filename.npy",c)
-
-    # loaded = np.load("filename.npy", allow_pickle=True)
-    # print(loaded)
-
     model = Game()
     mylist = []
     num = 10000
     i = 0
     n_game = 1
+    random_array = [3,4,5,6,7]
+    r_a = np.random.choice(random_array, 1, p=[0.15, 0.15, 0.4, 0.15, 0.15])
+    num_random = r_a[0]
+    list_element = []
     while(i < num):
         a = model.action_random_true()
         if(a[0] is None and a[1] is None):
             model.reset()
+            r_a = np.random.choice(random_array, 1, p=[0.15, 0.15, 0.4, 0.15, 0.15])
+            num_random = r_a[0]
             n_game+=1
+            if(num_random >= num - i):
+                # print("num_random: ", num_random)
+                # print("num - i: ", num - i)
+                # print("len(mylist): ", len(mylist))
+                # print("len(list_element): ", len(list_element))
+                # print("list_element: ", list_element[:(num - i)])
+                mylist.extend(list_element[:(num - i)])
+            else:
+                a2 = random.sample(list_element, num_random)
+                mylist.extend(a2)
+                list_element = []
+            i = len(mylist)
         else:
-            mylist.append(a)
-            i+=1
+            list_element.append(a)
+
     mat = np.array(mylist, dtype = object)
-    np.save("data10.npy",mat)
-    loaded = np.load("data10.npy", allow_pickle=True)
+    np.save("data.npy",mat)
+
+    loaded = np.load("data.npy", allow_pickle=True)
     print(loaded)
+    print(loaded.shape)
     print("n_game: ",n_game)
 
 
